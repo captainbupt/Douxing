3118856ee38e95d68f77514a06091b149590f1ae
Weizhou He <Weizhou He>
2015-07-18 02:58:33 -0400

    Developing the entry page

---
+++ app/src/main/java/com/badou/mworking/EntryActivity.java
@@ -0,0 +1,6 @@
+package com.badou.mworking;
+
+import com.badou.mworking.base.BaseNoTitleActivity;
+
+public class EntryActivity extends BaseNoTitleActivity{
+}
---
+++ app/src/main/java/com/badou/mworking/fragment/EntryIntroductionFragment.java
@@ -0,0 +1,6 @@
+package com.badou.mworking.fragment;
+
+import com.badou.mworking.base.BaseFragment;
+
+public class EntryIntroductionFragment extends BaseFragment{
+}
